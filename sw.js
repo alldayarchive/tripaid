717e523070c0ae27f0d844bbdf135db4adfae181
alldayarchive <halftotaketo@gmail.com>
2026-05-07 17:51:46 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,15 @@
-const CACHE_NAME = 'tripaid-v1';
+const CACHE_NAME = 'tripaid-v2';
 const ASSETS_TO_CACHE = [
   '/',
   '/index.html',
   '/privacy.html',
   '/terms.html',
   '/support.html',
+  '/manifest.json',
   'https://cdn.tailwindcss.com',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css'
 ];
 
-// 설치 시 캐시 저장
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
@@ -18,7 +18,6 @@ self.addEventListener('install', (event) => {
   );
 });
 
-// 네트워크가 안 될 때 캐시에서 꺼내오기
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
